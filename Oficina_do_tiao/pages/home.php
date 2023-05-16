7eeb399acf5c15f47c6b8e9ed3bec46aad24bcea
cainamicael <cainamicaeloficial@gmail.com>
2023-05-16 10:17:20 -0300

    add

--- Oficina_do_tiao/pages/home.php
+++ Oficina_do_tiao/pages/home.php
@@ -71,7 +71,7 @@
         </div><!--center-->
     </section><!--Especialidades-->
 
-        <div class="video">
+    <div class="video">
         <iframe class="video-iframe" width="560" height="315" src="https://www.youtube.com/embed/uuTwF2tFHKc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div><!--video-->
     
