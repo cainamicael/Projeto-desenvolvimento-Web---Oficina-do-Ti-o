9db83a387e819c54f7fc4518ecda473d3ab2f101
cainamicael <cainamicaeloficial@gmail.com>
2023-06-12 11:20:55 -0300

    add

--- Oficina_do_tiao/pages/contato.php
+++ Oficina_do_tiao/pages/contato.php
@@ -1,5 +1,5 @@
 <!--<div id="map"></div> Para o mapa via api-->
-<iframe id="mapa-iframe"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224430.4932972804!2d-80.72266556351728!3d28.487783458162095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e0a3f2a5fb9a45%3A0x87ef91ee250f8fde!2sCabo%20Canaveral!5e0!3m2!1spt-BR!2sbr!4v1682616061414!5m2!1spt-BR!2sbr" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<iframe id="mapa-iframe"src="https://www.google.com/maps/embed?pb=!4v1686579554258!6m8!1m7!1ssiv6r9pgXe9p2JBPJhGJ8g!2m2!1d-8.06616805419371!2d-34.93893737181532!3f32.721550534866154!4f-12.534407536342016!5f0.7820865974627469" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 <div class="contato-container">
 	<div class="center">
